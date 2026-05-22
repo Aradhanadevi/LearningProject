0b0246591b608801d24f7eff060616b00486368a
Aradhanadevi <aradhanajadeja81@gmail.com>
2026-05-23 00:15:08 +0530

    data types of C++

---
+++ CPlusPlus/Day2/practice.cpp
@@ -0,0 +1,19 @@
+//DataTypes
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 5; //Integer data type
+    float b = 3.14; //Floating-point data type
+    char c = 'A'; //Character data type 
+    bool d = true; //Boolean data type
+    cout << "Integer: " << a << endl;
+    cout << "Float: " << b << endl;
+    cout << "Character: " << c << endl;
+    cout << "Boolean: " << d << endl;
+    return 0;
+}
+
+//To declare datatypes in c++ you have to declare what type of data you want to store in a variable. For example, if you want to store an integer value, you can declare a variable of type int. If you want to store a floating-point value, you can declare a variable of type float. If you want to store a character, you can declare a variable of type char. If you want to store a boolean value, you can declare a variable of type bool.
