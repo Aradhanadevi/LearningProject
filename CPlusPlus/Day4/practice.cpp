cbac0d44c3a0e2620e83a42f62de60af21865a1e
Aradhanadevi <aradhanajadeja81@gmail.com>
2026-05-25 00:13:28 +0530

    .

--- CPlusPlus/Day4/practice.cpp
+++ CPlusPlus/Day4/practice.cpp
@@ -1,1 +1,13 @@
 //maybe unused attribute 
+//Today we will learn about the maybe_unused attribute in C++. The maybe_unused attribute is used to indicate that a variable, function, or parameter may not be used in the code. This can help to avoid compiler warnings about unused variables or functions.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+    int c = a + b;
+    cout << "The sum of a and b is: " << c << endl;
+    return 0;
+}
