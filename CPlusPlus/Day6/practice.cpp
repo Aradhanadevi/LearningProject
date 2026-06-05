cc67525b829a896dfd20978fbdf29f1a0efba15b
Aradhanadevi <aradhanajadeja81@gmail.com>
2026-06-05 17:37:02 +0530

    cin, cout

---
+++ CPlusPlus/Day6/practice.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int varInput{};
+    cout << "Hello, user please input a number: ";
+    cin >> varInput;
+    cout << "The number you have input is: " << varInput;
+    // cout << "\n";
+    return 0;
+}
