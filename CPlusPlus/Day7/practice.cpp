5219b749d53205623a27c70ad4b7ed4820c4b6d5
Aradhanadevi <aradhanajadeja81@gmail.com>
2026-06-07 22:43:38 +0530

    function

---
+++ CPlusPlus/Day7/practice.cpp
@@ -0,0 +1,13 @@
+//Function
+#include <iostream>
+using namespace std;
+
+//Function definition
+void printHello() {
+    cout << "Hello, World!" << endl;
+}
+
+int main() {
+    printHello();  //Function call
+    return 0;
+}
